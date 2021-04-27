6fa0e2f06dc7b9e2250103434a4098c604c8ccdf
Bimalesh Chauhan <developer.bimal@gmail.com>
2021-04-27 09:34:56 +0530

    update chnages

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require("body-parser");
+const cors = require("cors")
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors())
 const PORT = 4000;
 // app.use(json)
 const Quiz = require('./models/Quiz')
